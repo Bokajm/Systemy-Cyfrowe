8eb6b547ce15128eb806a7864deaeb86ff5312a0
Marcin Malcher <malcher@student.agh.edu.pl>
2022-04-03 15:57:34 +0200

    Implemented I2C reading
    Writing probably too, but not tested yet.

--- rekawica/i2c_hal.cpp
+++ rekawica/i2c_hal.cpp
@@ -1,7 +1,23 @@
 #include "i2c_hal.h"
-#include <avr/io.h>
+#include <Arduino.h>
 
 namespace i2c {
+constexpr uint8_t START = 0x08;
+constexpr uint8_t SLA_W_ACK_RECEIVED = 0x18;
+constexpr uint8_t DATA_ACK_RECEIVED = 0x28;
+constexpr uint8_t RESTART = 0x10;
+constexpr uint8_t SLA_R_ACK_RECEIVED = 0x40;
+
+namespace {
+template<typename... T>
+void log(T... t) {
+  ((Serial.println(t)), ...);
+}
+
+void logb(uint8_t b){
+  Serial.println(b, BIN);
+}
+}
 
 void init() {
   DDRD &= 0b11111100; //DDRXx = 0 -> pinx in port X set as input(data direction register)
@@ -13,7 +29,7 @@ void init() {
 }
 
 void wait() {
-  while (!(TWCR & (1 << TWINT))) ;
+  while (!(TWCR & (1 << TWINT)));
   //Wait until interrupt bit in control register becomes 0
 }
 
@@ -24,7 +40,63 @@ void start() {
   //set enable interface
 }
 
-uint8_t readAddr(uint8_t deviceAddr, uint8_t registerAddr) {
+bool checkStatus(uint8_t wantedStatus) {
+  return (TWSR & 0xF8) == wantedStatus;//Check if status bits are as expected
+}
 
+void sendByte(uint8_t data) {
+  TWDR = data;//Load data into data register
+  TWCR = (1 << TWINT) | (1 << TWEN); //Set int bit and enable bit to send
+}
+
+bool sendRegisterAddrToSlave(uint8_t deviceAddr, uint8_t registerAddr) {
+  start();
+  wait();
+  if (!checkStatus(START)) {
+    log("Error sending start condition");
+  }
+  sendByte((deviceAddr << 1) & 0xFE); //Send slave addr with write command
+  wait();
+  if (!checkStatus(SLA_W_ACK_RECEIVED)) {
+    Serial.println("Did not receive ACK on addr");
+    return false;
+  }
+  sendByte(registerAddr);
+  wait();
+  if (!checkStatus(DATA_ACK_RECEIVED)) {
+    Serial.println("Did not receive ACK on data");
+    return false;
+  }
+  return true;
+}
+
+void stop() {
+  TWCR = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN);
+}
+
+uint8_t readByteFromSlave(uint8_t deviceAddr) {
+  start();//Restart actually, but command is the same
+  wait();
+  if (!checkStatus(RESTART)) {
+    Serial.println("Error sending restart condition");
+  }
+  sendByte(deviceAddr << 1 | 1); //Slave addr with read command
+  wait();
+  if (!checkStatus(SLA_R_ACK_RECEIVED)) {
+    Serial.println("Didn't receive ACK on 2nd addr");
+  }
+
+  TWCR = 1 << TWINT | 1<<TWEN; //Let us receive data;
+  wait();
+  uint8_t receivedByte = TWDR;
+  stop();
+  return receivedByte;
+}
+
+uint8_t readFromAddr(uint8_t deviceAddr, uint8_t registerAddr) {
+  if (!sendRegisterAddrToSlave(deviceAddr, registerAddr)) {
+    return 0;
+  }
+  return readByteFromSlave(deviceAddr);
 }
 }// i2c
--- rekawica/rekawica.ino
+++ rekawica/rekawica.ino
@@ -5,9 +5,13 @@ void setup() {
   Serial.begin(115200);
   while (!Serial) ;
   i2c::init();
+  Serial.println("Init done");
+  delay(1000);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Serial.print("Received byte: ");
+  Serial.println(i2c::readFromAddr(0x68, 0x75), BIN);
+  delay(1000);
 }
